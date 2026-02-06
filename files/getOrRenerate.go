db79ec69261056abecc315aaf6a94aff029f48c9
sultepronit <sultepronit@gmail.com>
2026-02-06 18:26:13 +0000

    added male/female voice by record number algorithm

--- files/getOrRenerate.go
+++ files/getOrRenerate.go
@@ -1,14 +1,38 @@
 package files
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
 	"tts/service"
 	"tts/voices"
 )
 
+var rgxMp3 = regexp.MustCompile(`^([1-6])\.mp3$`)
+
+func getNum(record string) int {
+	matches := rgxMp3.FindStringSubmatch(record)
+	if len(matches) > 1 {
+		d, err := strconv.Atoi(matches[1])
+		if err != nil {
+			return -1
+		}
+		return d
+	}
+
+	return -1
+}
+
 func generate(expression string, record string) ([]byte, error) {
-	v, err := voices.GetRandomVoice(false)
+	d := getNum(record)
+	if d < 0 {
+		return nil, fmt.Errorf("invalid record: %s", record)
+	}
+
+	isMale := d%2 == 1
+	v, err := voices.GetRandomVoice(isMale)
 	if err != nil {
 		return nil, err
 	}
